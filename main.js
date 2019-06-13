47ba1faf60dc3dd4a50b2d5f6217fb6cbd4368d5
Satomin2864 <satomiyuta0602@gmail.com>
2019-06-14 00:55:23 +0900

    add: chap1, section5 fin

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 var app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        message: 'hello vue.js!',
+        list: ["りんご", "ゴリラ", "ラッパ"],
+        count: 0,
+        show: true,
+        methods: {
+            handleClick: function(event) {
+                alert(event.target)
+            }
+        }
+    }
 })
